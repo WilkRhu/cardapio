b8ec12ddeeedb3fc256e140c7c0c15a0c0d41768
WilkRhu <wilk.caetano@gmail.com>
2020-10-24 17:30:15 -0300

    feature: bulid

---
+++ build/precache-manifest.6d5972bb308ffa8388e1681eef9c18cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847cb38d62f49e3db9a0f5ee9ee35eb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d887f8d46685f92db9e",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ef6db249c89ef73f7ef8",
+    "url": "/static/css/main.e9bc364f.chunk.css"
+  },
+  {
+    "revision": "2d887f8d46685f92db9e",
+    "url": "/static/js/2.9a46812e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a46812e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6db249c89ef73f7ef8",
+    "url": "/static/js/main.3f889e73.chunk.js"
+  },
+  {
+    "revision": "607d2712066b5c4c3b58",
+    "url": "/static/js/runtime-main.96ee5432.js"
+  },
+  {
+    "revision": "694b3657a59e7f9eac96afc10321aea2",
+    "url": "/static/media/01.694b3657.png"
+  },
+  {
+    "revision": "4a9360e7857f62e9ed59287a46004caa",
+    "url": "/static/media/cabecalho.4a9360e7.jpg"
+  },
+  {
+    "revision": "b902f11c70442e729e8a42c1bd5d83d3",
+    "url": "/static/media/image18.b902f11c.png"
+  },
+  {
+    "revision": "4db600f2c0617df8034538f6950c112b",
+    "url": "/static/media/loading.4db600f2.gif"
+  }
+]);
